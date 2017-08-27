5762b79625718130f4199a115ca5f71f4f4a2319
Phil <phil@neto.com.au>
2017-08-27 12:15:51 +1000

    changed workflow from webpack to gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,21 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
 var fs = require('fs');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
+var vendors = {}
+vendors.src = './node_modules/';
+vendors.bootstrap = vendors.src + 'bootstrap/dist/js/bootstrap.js';
+vendors.fancybox = vendors.src + 'fancybox/dist/js/jquery.fancybox.js';
+vendors.jquery = vendors.src + 'jquery/dist/jquery.js';
+vendors.jqueryUI = vendors.src + 'jquery-ui-dist/jquery-ui.js';
+
+gulp.task('js', function() {
+		gulp.src([vendors.jquery, vendors.bootstrap, vendors.fancybox, vendors.jqueryUI])
+	    .pipe(concat('vendor.js'))
+	    .pipe(gulp.dest('./src/js'));
+});
 
 gulp.task('less', function () {
 	fs.stat('./src/css/less/app.less', function(err, stat) {
